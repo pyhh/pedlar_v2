b62a22e50ee53bda24c6379b4624f73554056d2c
Harry Hill <harryjhill@googlemail.com>
2020-02-22 12:59:16 +0000

    Csv reader for speed. Limit holdings. PEP8.

--- icalgodemo/bofa.py
+++ icalgodemo/bofa.py
@@ -1,139 +1,143 @@
+import csv
 import pandas as pd
 import numpy as np
-import pickle
 import os, time, sys
-
 
 class DataLoader():
 
     def __init__(self, filename='EURGBP.csv', source='csv'):
 
         self.source = source
-        self.chunksize = 100000
         self.counter = 0
         self.name = filename
-        self.hasdata = True
+        self.has_data = True
+        self.formats = {
+            'bid': lambda x: np.float(x),
+            'ask': lambda x: np.float(x),
+            'type': lambda x: np.int(x),
+            'time': lambda x: pd.to_datetime(x, format='%Y%m%d %H:%M:%S.%f'),
+        }
 
         if source == 'csv':
-            self.dataloader = pd.read_csv(filename, iterator=True)
+            self.data = csv.reader(open(filename))
+            self.columns = self.data.__next__()
 
     def next(self):
-        if self.source == 'csv':
-            if self.counter % self.chunksize == 0:
-                try:
-                    self.dfslice = self.dataloader.get_chunk(self.chunksize)
-                    data = self.dfslice.iloc[0, :].to_dict()
-                    self.counter = 1
-                except:
-                    data = None
-            else:
-                try:
-                    data = self.dfslice.iloc[self.counter, :].to_dict()
-                    self.counter += 1
-                except IndexError:
-                    data = None
-                    self.hasdata = False
+        try:
+            data = dict(zip(self.columns, self.data.__next__()))
+            for key, value in data.items():
+                if key in self.formats:
+                    data[key] = self.formats[key](value)
+        except (StopIteration, IndexError):
+            data = None
+            self.has_data = False
         return data
 
 
 class BOFAStrategy():
 
-    def __init__(self, tickdata='EURGBP.csv', signaldata='signal.csv', debug=False):
+    def __init__(self, tick_data='EURGBP.csv', signal_data='signal.csv', debug=False):
 
         self.pnl = []
         self.holdings = []
         self.period = []
-        self.startcash = 100
-        self.cash = self.startcash
+        self.start_cash = 10000
+        self.cash = self.start_cash
         self.holding = 0
         self.target = 0
-        self.lastbid = 0
-        self.lastask = 0
+        self.last_bid = 0
+        self.last_ask = 0
+        self.last_time = None
+        self.min_holding = -1000
+        self.max_holding = 1000
 
-        self.tickreader = DataLoader(filename=tickdata)
-        self.signalreader = DataLoader(filename=signaldata)
+        self.tick_reader = DataLoader(filename=tick_data)
+        self.signal_reader = DataLoader(filename=signal_data)
 
-        self.nexttick = self.tickreader.next()
-        self.nextsignal = self.signalreader.next()
+        self.next_tick = self.tick_reader.next()
+        self.next_signal = self.signal_reader.next()
 
     def valuation(self):
         if self.holding >= 0:
-            self.portfolio_value = self.holding * self.lastbid + self.cash
-            self.pnl.append(self.portfolio_value - self.startcash)
+            self.portfolio_value = self.holding * self.last_bid + self.cash
+            self.pnl.append(self.portfolio_value - self.start_cash)
             self.holdings.append(self.holding)
-            self.period.append(self.lasttime)
+            self.period.append(self.last_time)
         else:
-            self.portfolio_value = self.holding * self.lastask + self.cash
-            self.pnl.append(self.portfolio_value - self.startcash)
+            self.portfolio_value = self.holding * self.last_ask + self.cash
+            self.pnl.append(self.portfolio_value - self.start_cash)
             self.holdings.append(self.holding)
-            self.period.append(self.lasttime)
+            self.period.append(self.last_time)
         return None
 
     def rebalance(self):
         changes = self.target - self.holding
         if changes >= 0:
-            self.cash = self.cash - changes * self.lastask
+            self.cash = self.cash - changes * self.last_ask
             self.holding = self.target
         else:
-            self.cash = self.cash - changes * self.lastbid
+            self.cash = self.cash - changes * self.last_bid
             self.holding = self.target
         return None
 
     def run(self, debug=False):
         self.before_trades()
         # Compare data and signal time
-        while self.tickreader.hasdata or self.signalreader.hasdata:
-            if pd.to_datetime(self.nexttick['time'], infer_datetime_format=True) < pd.to_datetime(
-                    self.nextsignal['timestamp'], infer_datetime_format=True) or not self.signalreader.hasdata:
-                self.lasttime = pd.to_datetime(self.nexttick['time'], infer_datetime_format=True)
-                self.lastbid = np.float(self.nexttick['bid'])
-                self.lastask = np.float(self.nexttick['ask'])
+        while self.tick_reader.has_data and self.signal_reader.has_data:
+            if self.next_tick['time'] < self.next_signal['time'] or not self.signal_reader.has_data:
+                self.last_time = self.next_tick['time']
+                self.last_bid = self.next_tick['bid']
+                self.last_ask = self.next_tick['ask']
                 self.rebalance()
                 self.valuation()
-                self.target = self.ondata(self.lastbid, self.lastask)
+                new_target = self.on_tick(self.last_bid, self.last_ask)
+                if new_target is not None:
+                    self.target = np.clip(new_target, self.min_holding, self.max_holding)
                 if debug:
-                    print(
-                        'Time {} Bid {} Ask {} Target {} Portfolio {}'.format(self.lasttime, self.lastbid, self.lastask,
-                                                                              self.target, self.portfolio_value))
-                nexttick = self.tickreader.next()
-                if nexttick is not None:
-                    self.nexttick = nexttick
+                    print('Time {} | Tick   | Bid {} Ask {} Target {} Portfolio {}'.format(
+                        self.last_time, self.last_bid, self.last_ask, self.target, self.portfolio_value))
+                next_tick = self.tick_reader.next()
+                if next_tick is not None:
+                    self.next_tick = next_tick
             else:
-                self.lasttime = pd.to_datetime(self.nextsignal['timestamp'], infer_datetime_format=True)
-                self.onsignal(self.nextsignal['type'], self.nextsignal['value'])
+                self.last_time = self.next_signal['time']
+                new_target = self.on_signal(self.next_signal['type'], self.next_signal['value'])
+                if new_target is not None:
+                    self.target = np.clip(new_target, self.min_holding, self.max_holding)
                 if debug:
-                    print('Time {} Type {} Value {}'.format(self.lasttime, self.nextsignal['type'],
-                                                            self.nextsignal['value']))
-                nextsignal = self.signalreader.next()
-                if nextsignal is not None:
-                    self.nextsignal = nextsignal
+                    print('Time {} | Signal | Type {} Value {}'.format(
+                        self.last_time, self.next_signal['type'], self.next_signal['value']))
+                next_signal = self.signal_reader.next()
+                if next_signal is not None:
+                    self.next_signal = next_signal
 
     def download_results(self, filename):
         backtest = pd.DataFrame({'Profit': self.pnl, 'Holding': self.holdings}, index=self.period)
         backtest.to_csv('Backtest_{}.csv'.format(filename))
         return None
 
-        ########  Functions to be supplied by user ###############################################
+        ########  Functions to be supplied by user ########
 
     def before_trades(self):
         return None
 
-    def ondata(self, bid, ask):
-        holding = np.random.random()
+    def on_tick(self, bid, ask):
+        holding = self.holding
         return holding
 
-    def onsignal(self, signaltype, signaldata):
-        return None
+    def on_signal(self, signal_type, signal_value):
+        holding = self.holding
+        return holding
 
 
 if __name__ == '__main__':
 
     try:
-        tickdata = sys.argv[1]
-        signaldata = sys.argv[2]
+        tick_data = sys.argv[1]
+        signal_data = sys.argv[2]
     except IndexError:
-        print('Need to provide filenames for tick and signal data')
+        print('Need to provide file names for tick and signal data')
 
-    sample = BOFAStrategy(tickdata=tickdata, signaldata=signaldata)
+    sample = BOFAStrategy(tick_data=tick_data, signal_data=signal_data)
     sample.run(debug=True)
     sample.download_results('Algosoc2020')
