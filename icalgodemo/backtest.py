3b2883e71a825c09b7fb20e96e27ffad5202f214
Harry Hill <harryjhill@googlemail.com>
2020-02-23 21:01:03 +0000

    Added plotting to backtest

--- icalgodemo/backtest.py
+++ icalgodemo/backtest.py
@@ -1,9 +1,23 @@
 import sys, os
 import importlib
+import matplotlib.pyplot as plt
 
 TEST_DATA = 'data/EURGBP.csv'
 TEST_SIGNALS = 'data/signals.csv'
 OUT_PATH = 'results'
+
+
+def plot_sample(sample):
+    fig, (ax, ax2) = plt.subplots(2, 1)
+    ax.plot(sample.pnl, color='blue', label='pnl')
+    ax2.plot(sample.holdings, color='red', label='holding', linestyle='--')
+    ax2.set_xlabel('ticks')
+    ax.set_ylabel('Pnl')
+    ax2.set_ylabel('Holding')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('{}.png'.format(output_name))
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -17,10 +31,11 @@ if __name__ == '__main__':
             submission = importlib.import_module(module_path, package='pedlar_v2')
             sample = submission.Strategy(tick_data=TEST_DATA, signal_data=TEST_SIGNALS)
             sample.run()
-            output_file = '{}.csv'.format(os.path.join(OUT_PATH, submission.__name__))
-            sample.download_results(output_file)
+            output_name = '{}/{}'.format(OUT_PATH, submission.__name__, sample.pnl[-1])
+            sample.download_results('{}_{}.csv'.format(output_name, sample.pnl[-1]))
+            plot_sample(sample)
         except:
-            output_file = '{}.FAILED'.format(os.path.join(OUT_PATH, sys.argv[1]))
-            with open(output_file, 'w') as f:
+            output_name = '{}.FAILED'.format(os.path.join(OUT_PATH, sys.argv[1]))
+            with open(output_name, 'w') as f:
                 pass
 
--- icalgodemo/strategies/team2.py
+++ icalgodemo/strategies/team2.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 from pedlar_v2.icalgodemo.base import BaseStrategy
 
 
@@ -7,26 +8,23 @@ class Strategy(BaseStrategy):
     def on_tick(self, bid, ask):
         """
         This function is called after each tick.
-        To change the holding to N units long, return N
-        To change the holding to N units short, return -N
+        To change the holding to N units, return N
         :param bid: the current bid price
         :param ask: the current ask price
-        :return: a float in the range [-1,1] representing the desired holding
+        :return: a float in the range [-1000, 1000] representing the desired holding
         """
         holding = self.holding
-
         return holding
 
     def on_signal(self, signal_type, signal_value):
         """
         This function is called each time a new signal arrives.
-        To change the holding to N units long, return N
-        To change the holding to N units short, return -N
+        To change the holding to N units, return N
         :param signal_type: an integer representing the type of signal received
         :param signal_value: a string representing the value of the signal.
-        :return: a float in the range [-N,N] representing the desired holding
+        :return: a float in the range [-1000, 1000] representing the desired holding
         """
-        holding = self.holding
+        holding = np.random.randn()
         return holding
 
     def before_trades(self):
